182f610664cd5d1e5e512ea240f0df3a57f8f40e
Jesus Emmanuel Rodriguez Estrada <>
2019-12-06 18:48:19 -0800

    Modos de juego

--- core/src/com/mygame/pooa/manager/screen/GameMenu.java
+++ core/src/com/mygame/pooa/manager/screen/GameMenu.java
@@ -11,9 +11,13 @@ import com.mygame.pooa.MyGamePOOA;
 import com.mygame.pooa.screens.PlayScreen;
 
 /**
+ * SEPARADOR 3000 1.0
  * @author Abraham Medina Carrillo
  * @author Jesus Emmanuel Rodriguez Estrada
  * @author Alejandro Gonzalez Zepeda
+ * CORREO ELECTRONICO {alejandro.gonzalez96, abraham.medina.carrillo, emmanuel.rodriguez40 }"uabc.edu.mx
+ * UNIVERSIDAD AUTONOMA DE BAJA CALIFORNIA
+ * http://uabc.mx
  */
 
 public class GameMenu {
