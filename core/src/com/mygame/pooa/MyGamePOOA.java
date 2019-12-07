98b871efceed705bb44c6dfdd3fd8d5d9ceea47e
Jesus Emmanuel Rodriguez Estrada <>
2019-12-06 19:06:04 -0800

    Modos de juego

--- core/src/com/mygame/pooa/MyGamePOOA.java
+++ core/src/com/mygame/pooa/MyGamePOOA.java
@@ -1,3 +1,12 @@
+/**
+ *  SEPARADOR 3000 1.0
+ *  APLICACION PARA ESCRITORIO DE ENTRETENIMIENTO Y CONCIENTIZACION SOBRE EL RECICLAJE
+ *  AUTORES: Gonzalez Zepeda Alejandro, Medina Carrillo Abraham, Rodriguez Estrada Jesus Emmanuel
+ *  CORREO ELECTRONICO {alejandro.gonzalez96, abraham.medina.carrillo, emmanuel.rodriguez40 }@uabc.edu.mx
+ *  UNIVERSIDAD AUTONOMA DE BAJA CALIFORNIA
+ *  http://uabc.mx
+ */
+
 package com.mygame.pooa;
 
 import com.badlogic.gdx.Game;
@@ -9,9 +18,6 @@ import com.mygame.pooa.screens.PlayScreen;
 /**
  * Manejador de pantallas, se controla el estado de las mismas, cual se mira y renderiza, cual se destruye
  * ademas de contener el manejador de recursos y {@link com.badlogic.gdx.graphics.g2d.Batch} para dibujar
- * @author Abraham Medina Carrillo
- * @author Jesus Emmanuel Rodriguez Estrada
- * @author Alejandro Gonzalez Zepeda
  */
 
 public class MyGamePOOA extends Game {
