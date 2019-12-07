f8bb9b99686e7341c9a986706584c5aba96758af
Jesus Emmanuel Rodriguez Estrada <>
2019-12-06 19:03:06 -0800

    Modos de juego

--- desktop/src/com/mygame/pooa/desktop/DesktopLauncher.java
+++ desktop/src/com/mygame/pooa/desktop/DesktopLauncher.java
@@ -10,9 +10,7 @@
 /**
  *  SEPARADOR 3000 1.0
  *  APLICACION PARA ESCRITORIO DE ENTRETENIMIENTO Y CONCIENTIZACION SOBRE EL RECICLAJE
- *  @author Abraham Medina Carrillo
- *  @author Jesus Emmanuel Rodriguez Estrada
- *  @author Alejandro Gonzalez Zepeda
+ *  AUTORES: Gonzalez Zepeda Alejandro, Medina Carrillo Abraham, Rodriguez Estrada Jesus Emmanuel
  *  CORREO ELECTRONICO {alejandro.gonzalez96, abraham.medina.carrillo, emmanuel.rodriguez40 }@uabc.edu.mx
  *  UNIVERSIDAD AUTONOMA DE BAJA CALIFORNIA
  *  http://uabc.mx
